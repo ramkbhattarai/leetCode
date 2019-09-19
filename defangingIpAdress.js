eafa870a1945367a5dc14d48125160e91f32ccb3
Ram <ramkb466@gmail.com>
2019-09-19 17:11:56 -0400

    solved IP Address problem

---
+++ defangingIpAdress.js
@@ -0,0 +1,29 @@
+/**
+ * Given a valid (IPv4) IP address, return a defanged version of that IP address.
+
+A defanged IP address replaces every period "." with "[.]".
+
+
+
+Example 1:
+
+Input: address = "1.1.1.1"
+Output: "1[.]1[.]1[.]1"
+Example 2:
+
+Input: address = "255.100.50.0"
+Output: "255[.]100[.]50[.]0"
+ */
+
+var defangIPaddr = function (address) {
+    let ans = "";
+    for (let i = 0; i < address.length; i++) {
+        if (address.charAt(i) === ".") {
+            ans += '[.]'
+        } else {
+            ans += address.charAt(i);
+        }
+
+    }
+    return ans;
+};
