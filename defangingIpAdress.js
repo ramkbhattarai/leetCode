b2f8a8856fcb60545c608e1dac3cc4e7348e1862
Ram <ramkb466@gmail.com>
2019-09-19 17:20:34 -0400

    solved jewel and stone problem

--- defangingIpAdress.js
+++ defangingIpAdress.js
@@ -18,7 +18,7 @@ Output: "255[.]100[.]50[.]0"
 var defangIPaddr = function (address) {
     let ans = "";
     for (let i = 0; i < address.length; i++) {
-        if (address.charAt(i) === ".") {
+        if (address.charAt(i) === ".") { // we have to use this way to iterate through the string in javascript.
             ans += '[.]'
         } else {
             ans += address.charAt(i);
