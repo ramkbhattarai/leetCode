4f5057d51549a07c98b4695bf4eae0091b3930fc
Ram <ramkb466@gmail.com>
2019-09-28 10:19:02 -0400

    solution of merge sorted arrays

---
+++ mergeSortedArray.js
@@ -0,0 +1,40 @@
+/**
+ * Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+
+Note:
+
+The number of elements initialized in nums1 and nums2 are m and n respectively.
+You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2.
+Example:
+
+Input:
+nums1 = [1,2,3,0,0,0], m = 3
+nums2 = [2,5,6],       n = 3
+
+Output: [1,2,2,3,5,6]
+ */
+
+var merge = function (nums1, m, nums2, n) {
+    let i = m - 1;
+    let j = n - 1;
+    let k = m + n - 1;
+    while (i >= 0 && j >= 0) {// start from the end of the arrays since they are sorted
+        // last element will be the greatest in both so check which of there is greater and keep it in last of the nums1 array.
+        if (nums1[i] > nums2[j]) {
+            nums1[k] = nums1[i];
+            k--;
+            i--;
+        } else {
+            nums1[k] = nums2[j];
+            k--;
+            j--;
+        }
+    }
+
+    while (j >= 0) { // this is for test case if nums1= [0] and nums2 = [1]
+        nums1[k] = nums2[j];
+        k--;
+        j--;
+    }
+    return nums1;
+};
